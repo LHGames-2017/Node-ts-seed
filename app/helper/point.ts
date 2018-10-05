80db2f87cfc2ce30e3df736a748311e186d3e304
LHGames <42819896+LHGames-2018@users.noreply.github.com>
2018-10-05 12:38:48 -0400

    Added comment

--- app/helper/point.ts
+++ app/helper/point.ts
@@ -13,6 +13,8 @@ export class Point {
         return undefined;
     }
 
+    // Make sure to use Point.Equals(pt1, pt2) when comparing points
+    // and not pt1 === pt2.
     public static Equals(pt1: Point, pt2: Point): boolean {
         return pt1.x === pt2.x && pt1.y === pt2.y;
     }
